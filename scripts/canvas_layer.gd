83bfc74f6e125d0113400274b2942bab7470275e
corralwars <corralwars@gmail.com>
2026-09-19 12:49:01 -0500

    ended test

---
+++ scripts/canvas_layer.gd
@@ -0,0 +1,46 @@
+extends CanvasLayer
+
+var total_coins = 0
+var collected_coins = 0
+
+@onready var coins_label=$Control/LabelCoins
+@onready var timer_label=$Control/LabelTimer
+
+@export var start_time:float = 10.0
+var time_left: float
+
+func _ready():
+	var coins = get_tree().get_nodes_in_group("coins")
+	total_coins = coins.size()
+	_update_label_coins()
+	time_left = start_time
+	
+func _process(delta):
+	if time_left<=0.0:
+		get_tree().reload_current_scene()
+		
+	time_left-=delta
+	time_left=max(time_left,0.0)
+	timer_label.text="%.2f" % time_left
+	
+
+func _update_label_coins():
+	coins_label.text="%d / %d" %[collected_coins,total_coins]
+
+func coin_collected():
+	collected_coins+=1
+	if collected_coins == total_coins:
+		load_next_level()
+	_update_label_coins()
+		
+
+
+func load_next_level():
+	var current_scene=get_tree().current_scene
+	var current_name=current_scene.scene_file_path
+	
+	var level_number=int(current_name.get_file().get_basename().replace("level",""))
+	var next_level = level_number+1
+	
+	var next_path="res://scenes/Level%d.tscn" % next_level
+	get_tree().change_scene_to_file(next_path)
--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,4 +3,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		queue_free()
+		_collect()
+
+func _collect():
+	var ui = get_tree().get_first_node_in_group("ui")
+	if ui:
+		ui.coin_collected()
+	queue_free()
